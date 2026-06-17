62689d4eb64193cf26eba7fd4fb6a7328a08396f
Erdinus <134903766+erdthedev@users.noreply.github.com>
2026-06-17 15:10:23 +0700

    Set Supabase URL and anon key
    Updated Supabase configuration with new URL and anon key.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Isi dari Supabase Project Settings > API
 // Jangan pakai service_role key di frontend.
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = '';
+export const SUPABASE_URL = 'https://mxyywmwahbyqwotnxhyx.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14eXl3bXdhaGJ5cXdvdG54aHl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NzY1NTUsImV4cCI6MjA5NzI1MjU1NX0.KliNbpENVbXXWbdA_1rYfE1Mbv6TNDWBwAavCSlcroA';
